bb8cac8d960a941e44ba4d2f1446566de2265e4f
panshiyu <panshiyu@meituan.com>
2014-03-18 15:35:20 +0800

    道生一
    道生一，一生二，二生三，三生万物

---
+++ WPGhostCatching/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  WPGhostCatching
+//
+//  Created by psy on 14-3-18.
+//  Copyright psy 2014年. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ WPGhostCatching/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WPGhostCatching
+//
+//  Created by psy on 14-3-18.
+//  Copyright psy 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
